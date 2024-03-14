4865c0bfad84ad1b211e0fddae2abe065862887e
Rockay98 <filipepereira9855@gmail.com>
2024-03-14 16:48:25 +0000

    Trigger a “Game Over” message

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -18,9 +18,9 @@ public class NewBehaviourScript : MonoBehaviour
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        }
-        else if (transform.position.z < lowerBound)
+        }else if (transform.position.z < lowerBound)       
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
